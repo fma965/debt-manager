54224899bf517d8e060e8c3d6251c2e8d571c67f
Scott <admin@cyanlabs.net>
2022-09-26 22:36:06 +0100

    Remove uneeded functions

--- www/inc/functions.php
+++ www/inc/functions.php
@@ -16,29 +16,6 @@
 			return 0;
 		}
 	}
-	
-	// Formats a 'unix timecode' to a date
-	function formatDate($date) {
-		$date = gmdate("Ymd",$date);
-		$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-		$year = substr($date, 0, 4);
-		$month = $months[intval(substr($date, 4, 2))-1];
-		$day = (int)substr($date, -2);
-		return sprintf("%d %s %d", $day, $month, $year);
-	}
-	
-	// Function found on stackoverflow, used to get date suffix
-	function ordinal_suffix($num){
-		$num = $num % 100; // protect against large numbers
-		if($num < 11 || $num > 13){
-			 switch($num % 10){
-				case 1: return $num .'st';
-				case 2: return $num .'nd';
-				case 3: return $num .'rd';
-			}
-		}
-		return $num .'th';
-	}
 
 	function discord_notification($json,$discord_webhook) {
 		$ch = curl_init( $discord_webhook );
